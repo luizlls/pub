be3cb94fb978c4e97b1236ed457488b451d8c636
Luiz Lopes <luizlopesdev@gmail.com>
2018-11-25 21:56:36 -0200

    Basic app

---
+++ pub/app.py
@@ -0,0 +1,16 @@
+from aiohttp import web
+
+
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def hello(request):
+    return web.Response(text="Hello, world")
+
+
+def init_app():
+    app = web.Application()
+    app.add_routes(routes)
+
+    return app
